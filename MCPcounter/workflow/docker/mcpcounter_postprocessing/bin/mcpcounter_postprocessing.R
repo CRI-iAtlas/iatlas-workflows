20b08fea7653d32a19edda034d5ca492336ad80a
andrewelamb <andrewelamb@gmail.com>
2020-03-19 10:17:02 -0700

    create iatlas specific workflow

--- MCPcounter/workflow/docker/mcpcounter_postprocessing/bin/mcpcounter_postprocessing.R
+++ MCPcounter/workflow/docker/mcpcounter_postprocessing/bin/mcpcounter_postprocessing.R
@@ -4,25 +4,33 @@ library(tibble)
 library(magrittr)
 
 
-parser = ArgumentParser(description = "Deconvolute tumor samples with MCPcounter")
+parser = ArgumentParser(
+    description = "Deconvolute tumor samples with MCPcounter"
+)
 
 parser$add_argument(
     "--input_mcpcounter_file",
     type = "character",
     required = TRUE,
-    help = "Path to input mcpcpunter file.")
+    help = "Path to input mcpcpunter file."
+)
 parser$add_argument(
     "--output_file",
     default = "./output_file.tsv",
     type = "character",
-    help = "Path to output file.")
+    help = "Path to output file."
+)
 
 args = parser$parse_args()
 
 args$input_mcpcounter_file %>% 
     read.table(sep = "\t", stringsAsFactors = F) %>%
+    magrittr::set_rownames(
+        ., 
+        stringr::str_replace_all(rownames(.), " ", "_")
+    ) %>% 
     t %>% 
     as.data.frame() %>% 
     tibble::rownames_to_column("sample") %>% 
     tibble::as_tibble() %>% 
-    readr::write_tsv(args$output_file)
+    readr::write_tsv(., args$output_file)
